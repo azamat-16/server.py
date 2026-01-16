d471f2de98044cda5f98052ebeda909ffeddbd25
azamat-16 <azarzd16@gmail.com>
2026-01-16 08:10:18 +0600

    Update server.py

--- server.py
+++ server.py
@@ -1,22 +1,7 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/")
 def root():
     return {"status": "AI Friend server is running 🚀"}
-
-@app.post("/chat")
-def chat(message: dict):
-    user_message = message.get("message", "")
-    return {
-        "reply": f"🤖 Я AI Friend. Ты написал: {user_message}"
-    }
